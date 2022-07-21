56bda8415d0fe0d51c77679cc8df4de040fcee21
HammamiKhairi <hammamikhairi@gmail.com>
2022-07-21 23:33:21 +0100

    fixed a bug

--- latom/soang.py
+++ latom/soang.py
@@ -1,6 +1,6 @@
 from dataclasses import dataclass, field
 
-from constants import ILLEGAL_CHARS
+# from constants import ILLEGAL_CHARS
 
 
 @dataclass
@@ -12,7 +12,7 @@ class Soang:
   cover:str = None
   def __post_init__(self):
     #! self.title = legal_title.replace(" ", "_")
-    self.title = "".join([i for i in self.title if i not in ILLEGAL_CHARS])
+    # self.title = "".join([i for i in self.title if i not in ILLEGAL_CHARS])
     self.cover = self.cover.split("?")[0] if self.cover else None
 
   def __str__(self):
