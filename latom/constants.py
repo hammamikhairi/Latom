b006f289b2bbb2e4c8a7f599069c808e7494efbb
0xkhairi <95045924+hammamikhairi@users.noreply.github.com>
2022-07-26 14:52:49 +0100

    Optimization
    can be done using the os module :)

--- latom/constants.py
+++ latom/constants.py
@@ -1,10 +1,8 @@
 import os
-from multiprocessing import cpu_count
-
 
 ROOT = os.getcwd()
 PATH = f"/home/{os.getlogin()}/Music"
 ILLEGAL_CHARS = ["?", ">", "<", "|", ":", "/", "\\", "*"]
 
 # you can overclock the download by setting CORES to a high value
-CORES = cpu_count()
+CORES = os.cpu_count()
